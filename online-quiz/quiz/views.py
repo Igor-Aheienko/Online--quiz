dfcd450d78660067fcd7f4cc19c2a8057ca59e8c
Igor <agsertui@gmail.com>
2025-05-16 22:22:23 +0300

    Add question list and detail views with templates and answer checking logic

--- online-quiz/quiz/views.py
+++ online-quiz/quiz/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Question
+from .models import Question, Answer
 
 
 def question_list(request):
     questions = Question.objects.all()
     return render(request, 'quiz/question_list.html', {'questions': questions})
 
+
 def question_detail(request, pk):
     question = get_object_or_404(Question, pk=pk)
-    return render(request, 'quiz/question_detail.html', {'question': question})
+
+    result = None
+
+    if request.method == 'POST':
+        selected_id = request.POST.get('answer')
+        if selected_id:
+            selected_answer = Answer.objects.get(id=selected_id)
+            result = selected_answer.is_correct
+
+    return render(request, 'quiz/question_detail.html', {'question': question, 'result': result})
+    
